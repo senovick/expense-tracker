aa5fb751376ef2c557e3b3794dcb58f82f93ca29
Sean Novick <sean.novick.ctr@us.af.mil>
2020-03-30 12:30:16 -0600

    intial commit

---
+++ client/build/precache-manifest.7e496c527e9443fe8af6ef72973b62ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacdcb02c7e6f52d1e4a9fb946e7aeb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51256072614fbaf03f08",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "6463b30f185a98c0588f",
+    "url": "/static/js/2.1a604533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a604533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51256072614fbaf03f08",
+    "url": "/static/js/main.c4727767.chunk.js"
+  },
+  {
+    "revision": "675bf06bae219f1ad435",
+    "url": "/static/js/runtime-main.cd03f059.js"
+  }
+]);
